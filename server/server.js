ed51d548e0a254d5e1b191ce814c8c38cd004173
Tim Menzel <tim_menzel@t-online.de>
2022-01-18 11:33:30 +0100

    server test

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
-'use strict';
-
 const express = require('express');
+const app = express()
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(3000, () => {
+  console.log(`Running on 3000`);
+});
