76fd1c16c044d6502a276b117e8ce4d85a99fea5
Tim Menzel <tim_menzel@t-online.de>
2022-01-19 18:57:03 +0100

    server fix

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const io = new Server(httpServer, {
     origin: `*`,
     methods: ["GET", "POST"],
     allowedHeaders: ['*'],
-    credentials: true
+    credentials: false
   }
 });
 io.on("connection", (socket) => {
