b5c327225fbc95765fc0efc8bc2b2d33f2ec3362
Tim Menzel <tim_menzel@t-online.de>
2022-03-07 11:23:02 +0100

    fixed smool error with gotoObj

--- client/src/components/projectCluster.vue
+++ client/src/components/projectCluster.vue
@@ -3,7 +3,7 @@
     <p v-if="projects.length == 0">Looks quiet empty here</p>
     <div class="projectcluster_preview">
       <ProjectPreview class="projectcluster_project" :project="{ id: 'addProject' }" @mouseup="goToProject('-1')" v-if="hasAdd" />
-      <div class="projectcluster_project" v-for="proj in projects" :key="proj" @mouseup="goToProject(proj.projectID)">
+      <div class="projectcluster_project" v-for="proj in projects" :key="proj" @mouseup="goToProject(proj)">
         <ProjectPreview :project="{ id: proj }" />
         <div v-if="sharedbyself"></div>
         <p>{{ proj }}</p>
@@ -28,14 +28,20 @@ export default {
     sharedfrom: { type: Boolean, default: false },
   },
   methods: {
-    async goToProject(projectID) {
-      console.log("GOTOPROJ", projectID);
+    async goToProject(goto) {
+      let projectID;
+      console.log("HITIS", typeof goto)
+      if (typeof goto == "object") {
+        projectID = goto.projectID;
+      } else {
+        projectID = goto;
+      }
       const id = await api.projectcall("projects/getID", {
         SID: localStorage.getItem("sessionID"),
         user: localStorage.getItem("usr"),
         projectID: projectID,
       });
-      console.log("got to id", id)
+      console.log("got to id", id);
       if (toString(id).length == 18) {
         sessionStorage.setItem("projectID", id);
 
