b1552661f0ba0ab7dc39e9e0c21309be3869efc9
Tim Menzel <tim_menzel@t-online.de>
2022-03-11 09:35:48 +0100

    added privacy and impersum template

---
+++ client/src/views/PrivacyPolicy.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Privacy Policy</h1>
+
+    <h3>Collected Data</h3>
+    <ul>
+      <li>E-Mail address as a unique login ID</li>
+      <li>Self Chosen Username</li>
+      <li>Self Chosen Password</li>
+    </ul>
+    <p>The password got hashed first before it is stored</p>
+    <p>The E-Mails address is used to send Validations and inviting links -> This can be turned of</p>
+    <br />
+    <p>The account data can is stored until the manual deletion from the user itself</p>
+    <p>The data that is collected is used for login and sharing of projects data</p>
+    <p>The data is just for internal use and does not get transmitted to a third-party</p>
+    <p>This Website does not use any Tracking or Analyse software</p>
+    <p>This Website does not use any cookies</p>
+    <p>This Website does not transmit Systemdata</p>
+    <br />
+    <br />
+    <h3>Impresum</h3>
+    <p>Tim Menzel</p>
+    <p>Stöhnear Straße 3</p>
+    <p>04564 Böhlen</p>
+    <p>Germany</p>
+    <p>Phone: 0176 62929704</p>
+  </div>
+</template>
